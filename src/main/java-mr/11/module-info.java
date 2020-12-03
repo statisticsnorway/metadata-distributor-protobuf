923f1c92f782f0e5e87525bd03ce0cfb0a69a0b0
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-12-03 11:48:22 +0100

    Remove grpc dependencies

--- src/main/java-mr/11/module-info.java
+++ src/main/java-mr/11/module-info.java
@@ -1,10 +1,5 @@
 module no.ssb.dapla.metadata.distributor.protobuf {
-    requires com.google.common;
-    requires io.grpc;
-    requires grpc.stub;
     requires com.google.protobuf;
-    requires grpc.protobuf;
-    requires java.annotation;
 
     exports no.ssb.dapla.metadata.distributor.protobuf;
     opens no.ssb.dapla.metadata.distributor.protobuf;
